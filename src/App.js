67e35b6985035c2df212bcc9f76cb19a347b2352
shivamvora <shivampatel27102001@gmail.com>
2021-02-27 00:04:07 +0530

    useState done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
-
 function App() {
+  const [myName, setMyName] = useState( 'shivam vora', );
 
+  const changeName = () => {
+    setMyName( 'shivam vora web developer' );
+  }
   return (
     <div className="App">
-      <h1>react hooks</h1>
+      <h1>{myName}</h1>
+      <button onClick={changeName}>Click me</button>
     </div>
   );
 }
