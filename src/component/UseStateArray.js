ccb3821c899dfbf4e1c881c29b61c4218b405317
shivamvora <shivampatel27102001@gmail.com>
2021-02-27 19:22:19 +0530

    array with usestate

--- src/component/UseStateArray.js
+++ src/component/UseStateArray.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateArray = () => {
+    const bioData = [
+        {
+            id: 0,
+            myName: "shivam",
+            age: 20
+        },
+        {
+            id: 1,
+            myName: "jemin patel",
+            age: 28
+
+        }
+    ]
+    console.log( bioData );
+
+    const [myArray, setMyArray] = useState( bioData );
+
+    const clearArray = () => {
+        setMyArray( [] );
+    }
+
     return (
-        <div>
-            <h1>use state is called</h1>
-        </div>
+        <>
+            {
+                myArray.map( ( curElm ) => <h1 key={curElm.id}> id: {curElm.id}    Name: {curElm.myName} & Age: {curElm.age} </h1> )
+
+            }
+            <button onClick={clearArray}>clear</button>
+        </>
     )
 }
 
